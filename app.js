02bf74d29d73e1f1118131d4aaa2df070c5d8017
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2024-01-18 18:27:47 +0900

    No more bugs in the player select

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const gameContainer = document.querySelector(".container"),
-      userResult = document.querySelector(".user-result img"),
-      cpuResult = document.querySelector(".cpu-result img"),
+      userResult = document.querySelector(".user_result img"),
+      cpuResult = document.querySelector(".cpu_result img"),
       result = document.querySelector(".result"),
-      optionImages = document.querySelectorAll(".opt-img");
+      optionImages = document.querySelectorAll(".option_image");
+
 
 // Loop through each opt img
 optionImages.forEach((image, index) => {
-  image.addEventListener("click", () => {
+  image.addEventListener("click", (e) => {
     image.classList.add("active");
 
-    optionImages.forEach((img, index2) => {
-      console.log(index, index2);
-    })
-  })
-})
+    optionImages.forEach((image2, index2) => {
+      index !== index2 && image2.classList.remove("active");
+    });
+
+    let imgSrc = e.target.querySelector("img").src;
+    userResult.src = imgSrc;
+  });
+});
