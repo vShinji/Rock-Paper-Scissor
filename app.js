dcebcd6e89f7d72c85f422d5439d1c3f0af6c9be
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2024-01-18 19:14:04 +0900

    Game done

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const gameContainer = document.querySelector(".container"),
-      userResult = document.querySelector(".user_result img"),
-      cpuResult = document.querySelector(".cpu_result img"),
+      userResult = document.querySelector(".user-result img"),
+      cpuResult = document.querySelector(".cpu-result img"),
       result = document.querySelector(".result"),
-      optionImages = document.querySelectorAll(".option_image");
+      optionImages = document.querySelectorAll(".option-image");
 
 
 // Loop through each opt img
@@ -10,11 +10,51 @@ optionImages.forEach((image, index) => {
   image.addEventListener("click", (e) => {
     image.classList.add("active");
 
+    let randomQuestion = Math.floor(Math.random() * 4);
+    let questionImg = ["images/player-question.jpg", "images/cpu-question.jpg", "images/eminem-question.jpg", "images/cat-question.jpg"];
+
+    userResult.src = cpuResult.src = questionImg[randomQuestion];
+
+    result.textContent = "Wait...";
+
+
     optionImages.forEach((image2, index2) => {
       index !== index2 && image2.classList.remove("active");
     });
 
-    let imgSrc = e.target.querySelector("img").src;
-    userResult.src = imgSrc;
+    gameContainer.classList.add("start");
+
+    let time = setTimeout(() => {
+
+      gameContainer.classList.remove("start");
+
+      let imgSrc = e.target.querySelector("img").src;
+      userResult.src = imgSrc;
+
+      let randomNumber = Math.floor(Math.random() * 3);
+
+      let cpuImage = ["images/the-rock.gif", "images/the-paper.jpg", "images/the-scissor.jpg"];
+      cpuResult.src = cpuImage[randomNumber];
+
+      let cpuValue = ["R", "P", "S"][randomNumber];
+      let userValue = ["R", "P", "S"][index];
+
+      let results = {
+        RR: "Draw",
+        PP: "Draw",
+        SS: "Draw",
+        RP: "Cpu",
+        RS: "User",
+        PR: "User",
+        PS: "Cpu",
+        SR: "Cpu",
+        SP: "User",
+      };
+
+      let resultValues = results[userValue + cpuValue];
+
+      result.textContent = userValue === cpuValue ? "Match Draw" : `${resultValues} Won!`
+    },2500)
+
   });
 });
